c244731c48c519fe3348de52a813abd9c82cf8f8
TommyStarK <thomasmilox@gmail.com>
2019-03-26 15:09:43 +0100

    fix token malformation due to double quotes

--- use_cases/engine/pki/enable.sh
+++ use_cases/engine/pki/enable.sh
@@ -3,6 +3,8 @@
 
 DNS="example.com"
 ROOT_TOKEN=$(bash -c "cat vault.keys.json | jq .root_token")
+ROOT_TOKEN="${ROOT_TOKEN%\"}"
+ROOT_TOKEN="${ROOT_TOKEN#\"}"
 
 pki_engine_config()
 {
@@ -36,10 +38,7 @@ cat <<EOF
 EOF
 }
 
-echo $ROOT_TOKEN
-
 echo -e '[\033[0;32mHigh-Availability Vault\033[0m] Mounting Root PKI engine ...'
-# curl --header "X-Vault-Token: s.O5cZZlMnocsGq4141oHTl548" --request POST  --data "$(pki_engine_config)" http://127.0.0.1:8201/v1/sys/mounts/pki | jq
 curl --header "X-Vault-Token: $ROOT_TOKEN" \
     --request POST  \
     --data "$(pki_engine_config)" \
@@ -47,7 +46,6 @@ curl --header "X-Vault-Token: $ROOT_TOKEN" \
 echo -e '\n'
 
 echo -e '[\033[0;32mHigh-Availability Vault\033[0m] Generating root certificate ...'
-# curl --header "X-Vault-Token: s.O5cZZlMnocsGq4141oHTl548" --request POST  --data "$(root_certificate_authority_config)" http://127.0.0.1:8201/v1/pki/root/generate/internal | jq -r ".data.certificate" > CA_cert.crt
 curl --header "X-Vault-Token: $ROOT_TOKEN"  \
     --request POST \
     --data "$(root_certificate_authority_config)" \
@@ -56,7 +54,6 @@ curl --header "X-Vault-Token: $ROOT_TOKEN"  \
 echo -e '\n'
 
 echo -e '[\033[0;32mHigh-Availability Vault\033[0m] Configure CA and CRL URLs ...'
-# curl --header "X-Vault-Token: s.O5cZZlMnocsGq4141oHTl548" --request POST  --data "$(root_ca_crl_urls)" http://127.0.0.1:8201/v1/pki/config/urls | jq
 curl --header "X-Vault-Token: $ROOT_TOKEN"  \
     --request POST \
     --data "$(root_ca_crl_urls)" \
@@ -64,60 +61,60 @@ curl --header "X-Vault-Token: $ROOT_TOKEN"  \
 echo -e '\n'
 
 
-pki_engine_intermediate_config()
-{
-cat <<EOF
-{
-    "type":"pki", 
-    "config": { 
-        "max_lease_ttl":"43800h" 
-    }
-}
-EOF
-}
+# pki_engine_intermediate_config()
+# {
+# cat <<EOF
+# {
+#     "type":"pki", 
+#     "config": { 
+#         "max_lease_ttl":"43800h" 
+#     }
+# }
+# EOF
+# }
 
-echo -e '[\033[0;32mHigh-Availability Vault\033[0m] Mounting Intermediate PKI engine ...'
+# echo -e '[\033[0;32mHigh-Availability Vault\033[0m] Mounting Intermediate PKI engine ...'
 # curl --header "X-Vault-Token: s.O5cZZlMnocsGq4141oHTl548" --request POST  --data "$(pki_engine_intermediate_config)" http://127.0.0.1:8201/v1/sys/mounts/pki_int | jq
-curl --header "X-Vault-Token: $ROOT_TOKEN" \
-    --request POST  \
-    --data "$(pki_engine_intermediate_config)" \
-    http://127.0.0.1:8201/v1/sys/mounts/pki_int | jq
-echo -e '\n'
+# curl --header "X-Vault-Token: $ROOT_TOKEN" \
+#     --request POST  \
+#     --data "$(pki_engine_intermediate_config)" \
+#     http://127.0.0.1:8201/v1/sys/mounts/pki_int | jq
+# echo -e '\n'
 
-intermediate_certificate_authority_config()
-{
-cat <<EOF
-{
-    "common_name": "$DNS Intermediate Authority",
-    "ttl": "43800h"
-}
-EOF
-}
+# intermediate_certificate_authority_config()
+# {
+# cat <<EOF
+# {
+#     "common_name": "$DNS Intermediate Authority",
+#     "ttl": "43800h"
+# }
+# EOF
+# }
 
-echo -e '[\033[0;32mHigh-Availability Vault\033[0m] Generating intermediate csr ...'
+# echo -e '[\033[0;32mHigh-Availability Vault\033[0m] Generating intermediate csr ...'
 # curl --header "X-Vault-Token: s.O5cZZlMnocsGq4141oHTl548" --request POST  --data "$(intermediate_certificate_authority_config)"  http://127.0.0.1:8201/v1/pki_int/intermediate/generate/internal | jq -r ".data.csr" > intermediate.csr
-curl --header "X-Vault-Token: $ROOT_TOKEN" \
-    --request POST \
-    --data "$(intermediate_certificate_authority_config)" \
-    http://127.0.0.1:8201/v1/pki_int/intermediate/generate/internal | jq -r ".data.csr" > intermediate.csr
-echo -e '\n'
+# curl --header "X-Vault-Token: $ROOT_TOKEN" \
+#     --request POST \
+#     --data "$(intermediate_certificate_authority_config)" \
+#     http://127.0.0.1:8201/v1/pki_int/intermediate/generate/internal | jq -r ".data.csr" > intermediate.csr
+# echo -e '\n'
 
-payload_intermediate_cert()
-{
-cat <<EOF
-{
-  "csr": "@intermediate.csr"
-  "format": "pem_bundle"
-}
-EOF
-}
+# payload_intermediate_cert()
+# {
+# cat <<EOF
+# {
+#   "csr": "@intermediate.csr"
+#   "format": "pem_bundle"
+# }
+# EOF
+# }
 
-echo "$(payload_intermediate_cert)"
+# echo "$(payload_intermediate_cert)"
 
-echo -e '[\033[0;32mHigh-Availability Vault\033[0m] Siging intermediate certificate with root certificate ...'
+# echo -e '[\033[0;32mHigh-Availability Vault\033[0m] Siging intermediate certificate with root certificate ...'
 # curl --header "X-Vault-Token: s.O5cZZlMnocsGq4141oHTl548" --request POST --data "$(payload_intermediate_cert)" http://127.0.0.1:8201/v1/pki/root/sign-intermediate | jq -r '.data.certificate' > intermediate.cert.pem
-curl --header "X-Vault-Token: $ROOT_TOKEN" \
-    --request POST \
-    --data "$(payload_intermediate_cert)" \
-    http://127.0.0.1:8201/v1/pki/root/sign-intermediate | jq -r '.data.certificate' > intermediate.cert.pem
-echo -e '\n'
+# curl --header "X-Vault-Token: $ROOT_TOKEN" \
+#     --request POST \
+#     --data "$(payload_intermediate_cert)" \
+#     http://127.0.0.1:8201/v1/pki/root/sign-intermediate | jq -r '.data.certificate' > intermediate.cert.pem
+# echo -e '\n'
--- use_cases/init.sh
+++ use_cases/init.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-echo -e '[\033[0;32mHigh-Availability Vault\033[0m] Initializing HA Vault ...\n'
+echo -e '[\033[0;32mHigh-Availability Vault\033[0m] Initializing HA Vault ...'
 curl --request POST --data '{"secret_shares": 5, "secret_threshold": 3}' http://127.0.0.1:8201/v1/sys/init | jq  > vault.keys.json
 echo -e '\n'
 
@@ -10,7 +10,7 @@ UNSEAL_KEY_3=$(bash -c "cat vault.keys.json | jq .keys[2]")
 
 for (( i=1; i<4; i++ ))
 do  
-  echo -e "[\033[0;32mHigh-Availability Vault\033[0m] Unsealing Vault $i ...\n"
+  echo -e "[\033[0;32mHigh-Availability Vault\033[0m] Unsealing Vault $i ..."
   curl \
     --request POST \
     --data '{"key": '"$UNSEAL_KEY_1"'}' \
@@ -26,7 +26,7 @@ do
     --data '{"key": '"$UNSEAL_KEY_3"'}' \
     "http://127.0.0.1:820$i/v1/sys/unseal" > /dev/null 2>&1
 
- echo -e "[\033[0;32mHigh-Availability Vault\033[0m] Health check Vault $i ...\n"
+ echo -e "[\033[0;32mHigh-Availability Vault\033[0m] Health check Vault $i ..."
  curl "http://127.0.0.1:820$i/v1/sys/health" | jq
  echo -e '\n'
 done
