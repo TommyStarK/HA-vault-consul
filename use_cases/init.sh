78c1e3906144344921b25f9c50846376e08e052c
TommyStarK <thomasmilox@gmail.com>
2019-04-02 14:05:37 +0200

    wip: issuing certificate for intermediate CA

--- use_cases/init.sh
+++ use_cases/init.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 echo -e '[\033[0;32mHigh-Availability Vault\033[0m] Initializing HA Vault ...'
-curl --request POST --data '{"secret_shares": 5, "secret_threshold": 3}' http://127.0.0.1:8201/v1/sys/init | jq  > vault.keys.json
+curl --request POST --data '{"secret_shares": 5, "secret_threshold": 3}' http://127.0.0.1:8201/v1/sys/init | jq  > certs/vault.keys.json
 echo -e '\n'
 
-UNSEAL_KEY_1=$(bash -c "cat vault.keys.json | jq .keys[0]")
-UNSEAL_KEY_2=$(bash -c "cat vault.keys.json | jq .keys[1]")
-UNSEAL_KEY_3=$(bash -c "cat vault.keys.json | jq .keys[2]")
+UNSEAL_KEY_1=$(bash -c "cat certs/vault.keys.json | jq .keys[0]")
+UNSEAL_KEY_2=$(bash -c "cat certs/vault.keys.json | jq .keys[1]")
+UNSEAL_KEY_3=$(bash -c "cat certs/vault.keys.json | jq .keys[2]")
 
 sleep 2
 
